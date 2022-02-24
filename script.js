d28ffd18991221405e0eb75bdccd903d0ef4c166
cedcoss@nkit <ankitaginhotry@cedcoss.com>
2022-02-24 16:31:48 +0530

    Number gussing by jquery

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
-    $("#gen").click(function (e) { 
+    $("#guess").click(function (e) { 
         e.preventDefault();
-        var ch=$('#txt').val();
-        
-    if(ch==""){
-        alert("Required Feild");
-        $("#txt").css("borderColor","red");
+        var num=$("#num").val();
+    if(num==0 || num==""){
+        alert("Please Enter Number..");
     }
     else{
-        var ascii=ch.charCodeAt(0);
-        // document.getElementById('rs').innerHTML="ch==>"+ascii;
-        $("#rs").html("ASCII Code: "+ascii);
+    
+    var rd=Math.floor(Math.random() * 3);
+    console.log(rd);
+   
+    if(rd==num){
+        // document.getElementById('rs').innerHTML="You Guess Correct";
+        $("#rs").html("You Guess Correct");
     }
+    else{
+        // document.getElementById('rs').innerHTML="Ohh You Lose the Game!";
+        $("#rs").html("Ohh You Lose the Game!");
+
+
+    }
+}
     });
 });
+
