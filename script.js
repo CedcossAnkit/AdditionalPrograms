0ffa95391e9b4cffdfe1ae62b7e1d841d3c33788
cedcoss@nkit <ankitaginhotry@cedcoss.com>
2022-02-24 16:57:47 +0530

    boxes Genrator

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 $(document).ready(function () {
-    $("#guess").click(function (e) { 
+    $("#bt").click(function (e) { 
         e.preventDefault();
-        var num=$("#num").val();
-    if(num==0 || num==""){
-        alert("Please Enter Number..");
-    }
-    else{
-    
-    var rd=Math.floor(Math.random() * 3);
-    console.log(rd);
-   
-    if(rd==num){
-        // document.getElementById('rs').innerHTML="You Guess Correct";
-        $("#rs").html("You Guess Correct");
-    }
-    else{
-        // document.getElementById('rs').innerHTML="Ohh You Lose the Game!";
-        $("#rs").html("Ohh You Lose the Game!");
-
-
-    }
-}
+        console.log('Clicked');
+        var len=$("#txt").val();
+        var htm="";
+        for(var i=0;i<len;i++){
+            htm+="<div class='box'><p style='text-align:center'>"+i+"</p></div><br>"
+        }
+        $("#boxes").html(htm);
     });
 });
 
